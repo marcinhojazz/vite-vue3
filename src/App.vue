61c388b7d7a64a3baabb8ca75dbba8d110e70dc4
marcinhojazz <marcinhojazz@gmail.com>
2023-10-23 15:57:45 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useCounterStore } from './stores/CounterStore';
+
+const counter = useCounterStore();
+
+</script>
+
+<template>
+  <h2>Welcome to pinia</h2>
+  <div>
+    {{ counter.count }}
+    <button @click="counter.increment">Add</button>
+  </div>
+  
+</template>
