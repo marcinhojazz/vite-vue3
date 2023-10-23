b5659bb89501ffdf6fa636b0ef2042cae51af003
marcinhojazz <marcinhojazz@gmail.com>
2023-10-23 18:47:01 -0300

    comparison options api vs composition api

---
+++ src/components/CompositionAPI.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>Cart Composition API</h2>
+</template>
+
+<script setup>
+import { reactive, computed } from 'vue';
+
+// cart
+const cart = reactive(data:[]);
+function cartRemove(index) {
+  cart['data'].splice(index, 1);
+}
+const showCartInfo = computed(() => {
+  return "O carrinho de compras tem " + cart['data'].length + " produtos"
+})
+
+// product
+const products = reactive(data:[]);
+function productRemove(index) {
+  products['data'].splice(index, 1)
+}
+const showProductsInfo = computed(() => {
+  return "A lista de produtos tem " + cart['data'].length + " produtos";
+})
+
+</script>
---
+++ src/components/OptionsApi.vue
@@ -0,0 +1,34 @@
+<template>
+  <h2>Cart Options API</h2>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cart: [],
+      products: []
+    }
+  },
+  computed: {
+    showCartInfo() {
+      return "O carrinho de compras tem" + this.cart.length + "produtos";
+    },
+
+    showProductsInfo() {
+      return "A lista de produtos tem" + this.products.length + "produtos";
+    }
+  },
+  
+  methods: {
+    cartRemove(index) {
+      this.cart.splice(index, 1);
+    },
+
+    productRemove(index) {
+      this.products.splice(index, 1)
+    }
+  }
+}
+
+</script>
