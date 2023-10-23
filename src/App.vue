7c05628683ff57f37cbc4df3eb7cbd8e0ca8ee7e
marcinhojazz <marcinhojazz@gmail.com>
2023-10-23 19:02:00 -0300

    Organize space page for start add components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <script setup>
 import Counter from './components/Counter.vue'
-
+import TimeLine from './components/TimeLine.vue'
 </script>
 
 <template>
-  <h2>Welcome to pinia</h2>
-  <Counter />
+  <div>
+    <div>
+      <h2>Estudando Pinia + Composition API</h2>
+      <Counter />
+    </div>
+    <div>
+      <h2>Timeline Component DeviX'</h2>
+      <TimeLine /> 
+    </div>
+  </div>
   
 </template>
+
+<style>
+div {
+  display: grid;
+
+  gap: 22px;
+}
+</style>
